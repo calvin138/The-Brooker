30cb585a20890e4c65956d3d3e7d988734c98f73
Calvin Loh <calvin_loh138@yahoo.com>
2019-01-15 22:34:30 +0800

    Able to login

--- app/src/main/java/forum/student/thebrooker/SigninActivity.java
+++ app/src/main/java/forum/student/thebrooker/SigninActivity.java
@@ -1,13 +1,79 @@
 package forum.student.thebrooker;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SigninActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button signin;
+
+    private FirebaseAuth firebaseAuth;
+
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        setupUiViews();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+
+        progressDialog = new ProgressDialog(this);
+
+        if(user != null){
+            finish();
+            startActivity(new Intent(SigninActivity.this, MainActivity.class));
+        }
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(email.getText().toString(),password.getText().toString());
+            }
+        });
+    }
+
+    public void setupUiViews(){
+        email = (EditText)findViewById(R.id.et_Email1);
+        password = (EditText)findViewById(R.id.et_Password1);
+        signin = (Button)findViewById(R.id.btn_signin);
+    }
+
+    public void validate(String email, String password){
+        progressDialog.setMessage("Logging in");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    progressDialog.dismiss();
+                    Toast.makeText(SigninActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(SigninActivity.this, MainActivity.class));
+                }
+                else{
+                    Toast.makeText(SigninActivity.this, "Login Fail", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
